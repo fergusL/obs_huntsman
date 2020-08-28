81afe32d9945b667ecffec4576a854beb691d3c6
danjampro <danjampro@sky.com>
2020-08-28 11:17:44 +1000

    Include huntsman.drp.FitsHeaderTranslator (#21)
    * first
    
    * minor
    
    * asfafs
    
    * afw.geom to geom
    
    * get drp tests working
    
    Co-authored-by: danjmapro <danjmapro@sky.com>

--- python/lsst/obs/huntsman/ingest.py
+++ python/lsst/obs/huntsman/ingest.py
@@ -11,6 +11,8 @@ from lsst.utils import getPackageDir
 from lsst.pipe.tasks.ingest import IngestTask, ParseTask, IngestArgumentParser
 from lsst.pipe.tasks.ingestCalibs import CalibsParseTask
 
+from huntsman.drp.fitsutil import FitsHeaderTranslator
+
 
 class HuntsmanIngestArgumentParser(IngestArgumentParser):
 
@@ -30,67 +32,11 @@ class HuntsmanIngestTask(IngestTask):
     ArgumentParser = HuntsmanIngestArgumentParser
 
 
-class HuntsmanParseTask(ParseTask):
+class HuntsmanParseTask(ParseTask, FitsHeaderTranslator):
 
-    def translate_dataType(self, md):
-        """Translate FITS header into dataType: bias, flat or science."""
-
-        if md['IMAGETYP'] == 'Light Frame':
-            # The FIELD keyword is set by pocs.observation.field.field_name.
-            # For flat fields, this is "Flat Field"
-            if md["FIELD"].startswith("Flat Field"):
-                dataType = 'flat'
-            else:
-                dataType = 'science'
-        # For Huntsman, we treat all dark frames as biases.
-        # The exposure times are used to match biases with science images.
-        elif md['IMAGETYP'] == 'Dark Frame':
-            dataType = 'bias'
-        else:
-            raise NotImplementedError(f'IMAGETYP value not recongnised: '
-                                      f"{md['IMAGETYP']}")
-        return dataType
-
-    def translate_filter(self, md):
-        """
-        Translate the given filter name to the abstract filter name.
-        For Huntsman, we strip of the serial number.
-        """
-        return "_".join(md["FILTER"].split("_")[:-1])
-
-    def translate_dateObs(self, md):
-        """Return the date of observation as a string."""
-        return md['DATE-OBS'][:10]
-
-    def translate_visit(self, md):
-        """
-        Visit should be an integer value to avoid complications.
-
-        For Huntsman purposes, visit should be common to all exposures
-        taken simultaneously by the different cameras. This is encoded by the
-        time they were observed, provided there is sufficient temporal
-        resolution.
-
-        Unique exposures can therefore be identified by visit/ccd pairs.
-
-        Note: There needs to be space in memory for padding of the ccd number
-        used in computeExpId.
-        """
-        date_obs = md['DATE-OBS']  # This is a string
-        datestr = ''.join([s for s in date_obs if s.isdigit()])
-        assert len(datestr) == 17, "Date string expected to contain 17 numeric characters."
-        return int(datestr)
-
-    def translate_ccd(self, md):
-        """
-        Get a unique integer corresponding to the CCD.
-        """
-        ccd_name = md["INSTRUME"]
-        filename = os.path.join(getPackageDir("obs_huntsman"), "camera",
-                                "translate_ccd.yaml")
-        with open(filename, "r") as f:
-            ccd = int(yaml.safe_load(f)[ccd_name])
-        return ccd
+    def __init__(self, *args, **kwargs):
+        FitsHeaderTranslator.__init__(self)
+        ParseTask.__init__(self, *args, **kwargs)
 
 
 class HuntsmanCalibsParseTask(CalibsParseTask):
